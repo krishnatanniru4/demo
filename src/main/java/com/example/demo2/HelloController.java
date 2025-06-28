a50b8c5e6a5ba54bd8b05af94f8c749d26ac16fd
Krishna <krishna261.tanniru@gmail.com>
2025-06-28 10:54:06 -0500

    first commit

---
+++ src/main/java/com/example/demo2/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Deploy Spring Boot App using Elastic bean Stack";
+    }
+}
